cae9ab63b0a8c92335ecf041460268c35e86f0e8
KiryanovKD <kiryanovKD@yandex.ru>
2019-07-19 17:11:28 +0300

    memory bug fix

--- gtp_decoder.cpp
+++ gtp_decoder.cpp
@@ -28,26 +28,22 @@ void parse_file(std::string readPacketsFromPcapFileName, std::string savePackets
     pcpp::PcapFileWriterDevice writer(savePacketsToFileName.c_str());
 	writer.open();
 
-    // read the first (and only) packet from the file
     pcpp::RawPacket rawPacket;
-    u_int8_t *data;
-    pcpp::Layer *Eth;
 
     u_int8_t magic_bin = 0b00010000;
+
+    u_int8_t data[4000];
 
    
     while(reader.getNextPacket(rawPacket))
    	{
-
- 
     	pcpp::Packet parsedPacket(&rawPacket);
     	pcpp::Packet resultPacket(100);
 
     	if((*parsedPacket.getLastLayer()->getData() & magic_bin) == 0)
    			continue;
 
-
-    	Eth = parsedPacket.detachLayer(pcpp::Ethernet);
+        pcpp::Layer *Eth = parsedPacket.detachLayer(pcpp::Ethernet);
 
 
     	if(*(Eth->getData()+Eth->getDataLen()-2) != 0x81 || *(Eth->getData()+Eth->getDataLen()-1) != 0x00)
@@ -56,7 +52,8 @@ void parse_file(std::string readPacketsFromPcapFileName, std::string savePackets
         *(Eth->getData()+Eth->getDataLen()-2) = 0x08;
 
 
-        data = new u_int8_t[(int)parsedPacket.getLastLayer()->getDataLen()];
+        
+       // u_int8_t *data = new u_int8_t[(int)parsedPacket.getLastLayer()->getDataLen()];
         parsedPacket.getLastLayer()->copyData(data);
 
 
@@ -64,8 +61,8 @@ void parse_file(std::string readPacketsFromPcapFileName, std::string savePackets
 
         int ipv4_start = 0;
  		for(ipv4_start = 0; ipv4_start < (int)parsedPacket.getLastLayer()->getDataLen()-1; ipv4_start++){
- 			if(*(data+ipv4_start) == 0x45 && *(data+ipv4_start+1) == 0x00){
- 				pcpp::PayloadLayer newPayload((data+ipv4_start), ((int)parsedPacket.getLastLayer()->getHeaderLen()-ipv4_start), true);
+ 			if(data[ipv4_start] == 0x45 && data[ipv4_start+1] == 0x00){
+ 				pcpp::PayloadLayer newPayload(&data[ipv4_start], ((int)parsedPacket.getLastLayer()->getHeaderLen()-ipv4_start), true);
         		resultPacket.addLayer(Eth);
         		resultPacket.addLayer(&newPayload);
         		break;
@@ -74,17 +71,17 @@ void parse_file(std::string readPacketsFromPcapFileName, std::string savePackets
  		if(ipv4_start > (int)parsedPacket.getLastLayer()->getDataLen()-2)
  			continue;
 
+       // delete[] data;
+        //delete Eth;
 
- 		data = NULL;
- 		Eth = NULL;
+       // data = NULL;
+       // Eth = NULL;
 		writer.writePacket(*(resultPacket.getRawPacket()));
 
 	}
 
 
     // close the file
-    free(data);
-    free(Eth);
     reader.close();
     writer.close();
 
